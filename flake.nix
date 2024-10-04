9cd1e1b1e9e33f8c98b8715e03c8b95fbb2c5bc5
Gustavo L de Mello (Guz) <contact@guz.one>
2024-10-04 19:41:07 -0300

    chore: add golang tools to flake.niix

--- flake.nix
+++ flake.nix
@@ -2,11 +2,13 @@
   description = "My development environment";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    templ.url = "github:a-h/templ?ref=v0.2.778";
   };
   outputs = {
     self,
     nixpkgs,
-  }: let
+    ...
+  } @ inputs: let
     systems = [
       "x86_64-linux"
       "aarch64-linux"
@@ -18,14 +20,27 @@
         pkgs = import nixpkgs {inherit system;};
       in
         f system pkgs);
+    templ = system: inputs.templ.packages.${system}.templ;
   in {
     devShells = forAllSystems (system: pkgs: {
       default = pkgs.mkShell {
         buildInputs = with pkgs; [
+          # Javascript tools
           eslint
           nodejs_22
           nodePackages_latest.prettier
           bun
+          # Go tools
+          go
+          gofumpt
+          golangci-lint
+          golines
+          gotools
+          delve
+          (templ system)
+          # Sqlite tools
+          sqlite
+          lazysql
         ];
       };
     });
