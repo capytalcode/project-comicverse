09e1a0ce1e2237e9a1773695b4d70b583d8b022d
Gustavo L de Mello (Guz) <contact@guz.one>
2024-10-13 20:18:04 -0300

    chore(dev): eslint configuration
    I love Javascript :)

--- uno.config.js
+++ uno.config.js
@@ -1,45 +1,45 @@
 import {
-  defineConfig,
-  presetIcons,
-  presetTypography,
-  presetUno,
-  presetWebFonts,
-  transformerDirectives,
-  transformerVariantGroup,
-} from "unocss";
+	defineConfig,
+	presetIcons,
+	presetTypography,
+	presetUno,
+	presetWebFonts,
+	transformerDirectives,
+	transformerVariantGroup,
+} from 'unocss';
 
 export default defineConfig({
-  cli: {
-    entry: {
-      patterns: [
-        "./{pages,templates}/**/*.templ",
-        "./assets/**/*.{js,css,html}",
-        "!./assets/css/uno.css",
-      ],
-      outFile: "./assets/css/uno.css",
-    },
-  },
-  presets: [
-    presetIcons(),
-    presetTypography(),
-    presetUno(),
-    presetWebFonts({
-      provider: "bunny",
-      fonts: {
-        sans: {
-          name: "Inter",
-          provider: "none",
-        },
-        cal: {
-          name: "Cal Sans",
-          provider: "none",
-        },
-        mono: {
-          name: "Fira Code",
-          provider: "none",
-        },
-      },
-    }),
-  ],
-  transformers: [transformerDirectives(), transformerVariantGroup()],
+	cli: {
+		entry: {
+			outFile: './assets/css/uno.css',
+			patterns: [
+				'./{pages,templates}/**/*.templ',
+				'./assets/**/*.{js,css,html}',
+				'!./assets/css/uno.css',
+			],
+		},
+	},
+	presets: [
+		presetIcons(),
+		presetTypography(),
+		presetUno(),
+		presetWebFonts({
+			fonts: {
+				cal: {
+					name: 'Cal Sans',
+					provider: 'none',
+				},
+				mono: {
+					name: 'Fira Code',
+					provider: 'none',
+				},
+				sans: {
+					name: 'Inter',
+					provider: 'none',
+				},
+			},
+			provider: 'bunny',
+		}),
+	],
+	transformers: [transformerDirectives(), transformerVariantGroup()],
 });
