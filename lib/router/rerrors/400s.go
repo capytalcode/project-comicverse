f27784a0b277017c57bcf395d3c5f2cb34f8df71
Gustavo L de Mello (Guz) <contact@guz.one>
2024-10-24 19:35:43 -0300

    refactor(router,errors): move functiosn to alphabetical order

--- lib/router/rerrors/400s.go
+++ lib/router/rerrors/400s.go
@@ -1,15 +1,13 @@
 package rerrors
 
-import "net/http"
+import (
+	"net/http"
+)
+
+}
 
 func NotFound() RouteError {
 	return NewRouteError(http.StatusNotFound, "Not Found", map[string]any{})
-}
-
-func MissingParameters(params []string) RouteError {
-	return NewRouteError(http.StatusBadRequest, "Missing parameters", map[string]any{
-		"missing_parameters": params,
-	})
 }
 
 func MissingCookies(cookies []string) RouteError {
@@ -22,5 +20,11 @@ func MethodNowAllowed(method string, allowedMethods []string) RouteError {
 	return NewRouteError(http.StatusMethodNotAllowed, "Method not allowed", map[string]any{
 		"method":          method,
 		"allowed_methods": allowedMethods,
+	})
+}
+
+func MissingParameters(params []string) RouteError {
+	return NewRouteError(http.StatusBadRequest, "Missing parameters", map[string]any{
+		"missing_parameters": params,
 	})
 }
