eb72bab8863ed6c1f2f9042db2533b47da6e4cc7
Gustavo "Guz" L de Mello <contact@guz.one>
2025-05-20 10:12:02 -0300

    feat(ipub,ast): image element

--- ipub/ast/content.go
+++ ipub/ast/content.go
@@ -2,7 +2,7 @@ package ast
 
 import (
 	"encoding/xml"
-	"io"
+	"slices"
 )
 
 type Content struct {
@@ -27,3 +27,46 @@ func (e *Content) UnmarshalXML(dec *xml.Decoder, start xml.StartElement) error {
 	return e.UnmarshalXMLElement(e, dec, start)
 }
 
+type Image struct {
+	src string
+
+	BaseElement
+}
+
+var KindImage = NewElementKind("image", &Image{})
+
+func (e *Image) Name() ElementName {
+	return ElementName{Local: "img"}
+}
+
+func (e *Image) Kind() ElementKind {
+	return KindImage
+}
+
+func (e *Image) MarshalXML(enc *xml.Encoder, start xml.StartElement) error {
+	if src := e.Source(); src != "" {
+		start.Attr = append(start.Attr, xml.Attr{
+			Name:  xml.Name{Local: "src"},
+			Value: src,
+		})
+	}
+	return e.MarshalXMLElement(e, enc, start)
+}
+
+func (e *Image) UnmarshalXML(dec *xml.Decoder, start xml.StartElement) error {
+	i := slices.IndexFunc(start.Attr, func(a xml.Attr) bool {
+		return a.Name == xml.Name{Local: "src"}
+	})
+	if i > -1 {
+		e.SetSource(start.Attr[i].Value)
+	}
+	return e.UnmarshalXMLElement(e, dec, start)
+}
+
+func (e Image) Source() string {
+	return e.src
+}
+
+func (e *Image) SetSource(src string) {
+	e.src = src
+}
